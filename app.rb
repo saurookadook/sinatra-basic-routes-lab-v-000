07226e62efe54d6354728e48f9066d96f4d50ac7
Andy Maskiell <maskiella@gmail.com>
2018-02-06 21:38:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Andy"
+  end
+
+  get '/hometown' do
+    "My hometown is Rochester, NY."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Der Erlkonig by Franz Schubert"
+  end
 end
